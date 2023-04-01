4d44d5237365181756bd6a4d2907096b90919f05
Pc-Useh <cop0028240@est.univalle.edu>
2023-04-01 03:53:55 -0400

    Update config sqlserver to mysql.

--- Incuvalab/server/src/config.js
+++ Incuvalab/server/src/config.js
@@ -3,9 +3,9 @@ config();
 //Variable de Etorno
 export default {
     port: process.env.PORT || 4000,
-    dbUser: process.env.DB_USER || 'sa',
-    dbPort: process.env.DB_PORT || 1433,
-    dbPassword: process.env.DB_PASS || '354123',
+    dbUser: process.env.DB_USER || 'root',
+    dbPort: process.env.DB_PORT || 3306,
+    dbPassword: process.env.DB_PASS || '',
     dbServer: process.env.DB_SERVER || 'localhost',
-    dbDatabase: process.env.DB || 'DBIncuvalab',
+    dbDatabase: process.env.DB || 'dbincuvalab',
 }
