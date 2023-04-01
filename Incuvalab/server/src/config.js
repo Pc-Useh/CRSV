9463b4b1a5b7e8304f7848d3aaf6e129aadcfd26
Pc-Useh <cop0028240@est.univalle.edu>
2023-04-01 19:44:09 -0400

    dbport test.

--- Incuvalab/server/src/config.js
+++ Incuvalab/server/src/config.js
@@ -4,7 +4,7 @@ config();
 export default {
     port: process.env.PORT || 4000,
     dbUser: process.env.DB_USER || 'sa',
-    dbPort: process.env.DB_PORT || 1433,
+    dbPort: process.env.DB_PORT || 4022,
     dbPassword: process.env.DB_PASS || '12345678',
     dbServer: process.env.DB_SERVER || 'localhost',
     dbDatabase: process.env.DB || 'DbIncuvalab',
