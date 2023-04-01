b2c2e8117c6ec436e8c24c709b652f40da5465e4
Pc-Useh <cop0028240@est.univalle.edu>
2023-04-01 05:27:54 -0400

    Retorno variables base a sqlserver.

--- Incuvalab/server/src/config.js
+++ Incuvalab/server/src/config.js
@@ -3,9 +3,9 @@ config();
 //Variable de Etorno
 export default {
     port: process.env.PORT || 4000,
-    dbUser: process.env.DB_USER || 'root',
-    dbPort: process.env.DB_PORT || 3306,
-    dbPassword: process.env.DB_PASS || '',
+    dbUser: process.env.DB_USER || 'sa',
+    dbPort: process.env.DB_PORT || 1433,
+    dbPassword: process.env.DB_PASS || '12345678',
     dbServer: process.env.DB_SERVER || 'localhost',
-    dbDatabase: process.env.DB || 'dbincuvalab',
+    dbDatabase: process.env.DB || 'DbIncuvalab',
 }
