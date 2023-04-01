5c56b94aa53d5d5dd667ccf570e94a4cc78f6b79
Pc-Useh <cop0028240@est.univalle.edu>
2023-04-01 19:49:09 -0400

    config update.

--- Incuvalab/server/src/config.js
+++ Incuvalab/server/src/config.js
@@ -2,10 +2,10 @@ import { config } from 'dotenv'
 config();
 //Variable de Etorno
 export default {
-    port: process.env.PORT || 4000,
-    dbUser: process.env.DB_USER || 'sa',
-    dbPort: process.env.DB_PORT || 4022,
-    dbPassword: process.env.DB_PASS || '12345678',
-    dbServer: process.env.DB_SERVER || 'localhost',
-    dbDatabase: process.env.DB || 'DbIncuvalab',
+    port: (process.env.PORT || 4000),
+    dbUser: (process.env.DB_USER || 'sa'),
+    dbPort: (process.env.DB_PORT || 4022),
+    dbPassword: (process.env.DB_PASS || '12345678'),
+    dbServer: (process.env.DB_SERVER || 'localhost'),
+    dbDatabase: (process.env.DB || 'DbIncuvalab'),
 }
